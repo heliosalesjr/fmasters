612a17478cfd884f718faebe5334884b0f2d9f8e
heliosalesjr <heliosalesjr@gmail.com>
2024-07-18 14:49:18 -0300

    JS Projects starter - dark mode

--- copilot.js
+++ copilot.js
@@ -1,5 +1,25 @@
-function calculateDaysBetweenDates(begin, end) {
-  return Math.round((end - begin) / (1000 * 60 * 60 * 24));
-}
 
-console.log(calculateDaysBetweenDates())
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Digite o primeiro número: ', (num1) => {
+    rl.question('Digite o segundo número: ', (num2) => {
+        console.log(primos(num1, num2));
+        rl.close();
+    });
+    }
+);
+
+function primos(num1, num2) {
+  let primos = [];
+  for (let i = num1; i <= num2; i++) {
+    if (isPrimo(i)) {
+      primos.push(i);
+    }
+  }
+  return primos;
+}
